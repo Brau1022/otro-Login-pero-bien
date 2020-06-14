0ead3b17930a21151b29655dea55a164059239ff
Braulio Laptop <braulioraul22@hotmail.com>
2020-06-14 19:02:45 -0400

    Admin Menu clickable

--- app/src/main/java/com/example/ejemplologin/MainActivity.java
+++ app/src/main/java/com/example/ejemplologin/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.ejemplologin;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -17,14 +19,47 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    GridLayout mainGrid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainGrid = (GridLayout)findViewById(R.id.mainGrid);
+        setSingleEvent(mainGrid);
+    }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        for (int i=0; i<mainGrid.getChildCount(); i++){
+
+            CardView cardView = (CardView)mainGrid.getChildAt(i);
+            final int finalI = i;
+
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(MainActivity.this, "Click" + finalI, Toast.LENGTH_SHORT).show();
+                    if (finalI == 3){
+                        Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+                        logout();
+                    }else if (finalI == 2){
+                        Toast.makeText(MainActivity.this, "Tabla Ganancias Totales", Toast.LENGTH_SHORT).show();
+                    }else if (finalI == 1){
+                        Toast.makeText(MainActivity.this, "Tabla Vehiculos", Toast.LENGTH_SHORT).show();
+                    }else if (finalI == 0){
+                        Toast.makeText(MainActivity.this, "Tabla Choferes", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+            });
+
+        }
     }
 
     //logout process
-    public void logout(View view) {
+    public void logout() {
         FirebaseAuth.getInstance().signOut();
         GoogleSignIn.getClient(this, new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build())
                 .signOut().addOnSuccessListener(new OnSuccessListener<Void>() {
